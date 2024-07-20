0af8556ac1193f9e52f22e3d52bf5cb5592abe99
Cenan Bakıroğlu <120706481+cenanbakiroglu@users.noreply.github.com>
2024-07-20 15:52:08 +0300

    Add files via upload

---
+++ ikilik_taban/ikilik_taban/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ikilik_taban
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            ArrayList bit = new ArrayList();
+            Console.Write("İkilik tabana dönüştürülecek sayı :");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+             
+            while (sayi != 1 && sayi!=0 )
+            {
+                int kalan, sonuc;
+                sonuc = sayi / 2;
+                kalan = sayi - (sonuc * 2);
+                bit.Add(kalan);
+                sayi = sonuc;
+            } 
+            bit.Add(sayi);
+            ArrayList onluk = new ArrayList();
+            for(int i = (bit.Count-1);i>=0;i--)
+            {
+                onluk.Add ( Convert.ToInt32(bit[i]) * Math.Pow(2,i));
+            }
+
+            bit.Reverse();
+            Console.Write("Sayının ikilik tabanda gösterimi = ");
+            for(int i = 0;i<bit.Count;i++)
+            {
+                Console.Write(bit[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ikilik_taban/ikilik_taban/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ikilik_taban")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ikilik_taban")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a1a1e171-f5d0-40be-8369-d09966b3b343")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
